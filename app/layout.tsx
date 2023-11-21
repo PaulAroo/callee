e487129d34cb36368fad26553a5f309e543265e7
PaulAroo <timmypaul2006@gmail.com>
2023-11-21 13:15:55 +0100

    setup: chakra UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from "@/lib/Providers"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body>
+				<Providers>{children}</Providers>
+			</body>
 		</html>
 	)
 }
---
+++ lib/Providers.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { ChakraProvider } from "@chakra-ui/react"
+import { CacheProvider } from "@chakra-ui/next-js"
+
+export function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<CacheProvider>
+			<ChakraProvider>{children}</ChakraProvider>
+		</CacheProvider>
+	)
+}
