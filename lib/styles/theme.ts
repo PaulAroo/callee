f05bcfbc194f08af293b39dcf2ff651454fd73e8
PaulAroo <timmypaul2006@gmail.com>
2023-11-22 14:55:30 +0100

    add: more theme configs

--- lib/styles/theme.ts
+++ lib/styles/theme.ts
@@ -3,6 +3,20 @@ import "@fontsource-variable/nunito"
 import { extendTheme } from "@chakra-ui/react"
 
 const theme = extendTheme({
+	colors: {
+		brand: {
+			darkGrey: "#1C1D22",
+			purple: "#A000EB",
+		},
+	},
+	styles: {
+		global: {
+			body: {
+				bg: "brand.darkGrey",
+				color: "white",
+			},
+		},
+	},
 	fonts: {
 		body: `'Nunito Variable', sans-serif`,
 	},
