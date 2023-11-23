e984f7a42d436f2ddb9fef652d74bb5974c3a569
PaulAroo <timmypaul2006@gmail.com>
2023-11-23 16:31:18 +0100

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import { ChakraBaseProvider } from "@chakra-ui/react"
+import { RouterProvider } from "react-router-dom"
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import HomePage from "./pages"
-import SignInPage from "./pages/signin"
+import { router } from "./main"
+
 import theme from "../lib/styles/theme"
-import SignUpPage from "./pages/signup"
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <HomePage />,
-	},
-	{
-		path: "/signin",
-		element: <SignInPage />,
-	},
-	{
-		path: "/signup",
-		element: <SignUpPage />,
-	},
-])
 
 function App() {
 	return (
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,30 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import { ColorModeScript } from "@chakra-ui/react"
 import theme from "../lib/styles/theme.ts"
+import { createBrowserRouter } from "react-router-dom"
+import HomePage from "./pages/index.tsx"
+import SignInPage from "./pages/signin.tsx"
+import SignUpPage from "./pages/signup.tsx"
+
+export const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <HomePage />,
+		},
+		{
+			path: "/signin",
+			element: <SignInPage />,
+		},
+		{
+			path: "/signup",
+			element: <SignUpPage />,
+		},
+	],
+	{
+		basename: "/callee",
+	}
+)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
