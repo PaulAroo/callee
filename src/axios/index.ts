ccc30d84adf8ae78f66a5b32c64e46270e9a7ee7
PaulAroo <timmypaul2006@gmail.com>
2023-11-26 20:22:52 +0100

    configure: axios interceptors for auth headers

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,5 +1,19 @@
 import axios from "axios"
+import { LocalStorage } from "../utils"
 
-export default axios.create({
+const axiosInstance = axios.create({
 	baseURL: "https://klusterthon2023-production.up.railway.app/api",
 })
+
+axiosInstance.interceptors.request.use(
+	function (config) {
+		const token = LocalStorage.get("user").token
+		config.headers.Authorization = `Bearer ${token}`
+		return config
+	},
+	function (error) {
+		return Promise.reject(error)
+	}
+)
+
+export default axiosInstance
