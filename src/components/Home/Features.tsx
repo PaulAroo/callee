721a756ed09868a5822c2ee65cf80f5e8ca85301
PaulAroo <timmypaul2006@gmail.com>
2024-01-07 10:12:32 +0100

    style: landing page features section

--- src/components/Home/Features.tsx
+++ src/components/Home/Features.tsx
@@ -1,35 +1,87 @@
-import { Container, Flex, HStack, Image, Text, VStack } from "@chakra-ui/react"
+import {
+	Box,
+	Container,
+	Flex,
+	HStack,
+	Image,
+	Text,
+	VStack,
+} from "@chakra-ui/react"
 import MarkerOne from "../icons/MarkerOne"
 
 import callHistoryImage from "../../assets/Call_History_Page.png"
 import ongoingCallImage from "../../assets/Ongoing_call_Page.png"
 import incomingCallImage from "../../assets/Inoming_call_Page.png"
+import { motion } from "framer-motion"
 
 function Features() {
 	return (
-		<section>
+		<Box as="section" py={{ base: "4rem", lg: "8rem" }}>
 			<Container maxW="container.xl">
-				<VStack>
+				<VStack
+					gap={{ base: "3rem", md: "0px" }}
+					maxW={"64.1rem"}
+					marginInline={"auto"}
+				>
 					{FEATURES.map((f) => (
 						<Flex
 							key={f.title}
-							direction="column-reverse"
-							border="1px solid"
+							as={motion.div}
+							initial={{
+								opacity: 0,
+								x: f.align === "right" ? 100 : -100,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							viewport={{ amount: 0.3, once: true }}
+							transition="0.4s linear"
+							direction={{
+								base: "column-reverse",
+								md: `${f.align === "right" ? "row-reverse" : "row"}`,
+							}}
 							alignItems="center"
+							gap={"2rem"}
+							alignSelf={{
+								md: `${f.align === "right" ? "flex-end" : "flex-start"}`,
+							}}
+							mt={`${f.negativeMargin ? "-3rem" : "initial"}`}
 						>
 							<Image src={f.srcImage} />
 							<VStack>
-								<HStack>
-									<MarkerOne />
-									<Text as={"h1"}>{f.title}</Text>
+								<HStack
+									alignSelf={"start"}
+									flexDirection={{
+										md: `${f.align === "right" ? "row-reverse" : "row"}`,
+									}}
+								>
+									<Box fontSize={{ base: "2.5rem", lg: "3rem" }}>
+										<MarkerOne />
+									</Box>
+									<Text
+										as={"h1"}
+										fontSize={{ base: "1.5rem", lg: "2.3rem" }}
+										fontWeight="600"
+									>
+										{f.title}
+									</Text>
 								</HStack>
-								<Text>{f.desc}</Text>
+								<Text
+									maxW={{ base: "21rem", lg: "27rem" }}
+									fontSize={{ base: "1rem", lg: "1.5rem" }}
+									pl={{
+										base: "1rem",
+										md: `${f.align === "right" ? "0px" : "1rem"}`,
+									}}
+									fontWeight="400"
+									alignSelf={{ lg: "start" }}
+								>
+									{f.desc}
+								</Text>
 							</VStack>
 						</Flex>
 					))}
 				</VStack>
 			</Container>
-		</section>
+		</Box>
 	)
 }
 
@@ -38,6 +90,7 @@ interface Feature {
 	desc: string
 	srcImage: string
 	align: "left" | "right"
+	negativeMargin?: boolean
 }
 
 const FEATURES: Array<Feature> = [
@@ -52,12 +105,14 @@ const FEATURES: Array<Feature> = [
 		desc: "Instantly see text translations during calls. Enhance understanding for clear communication.",
 		srcImage: ongoingCallImage,
 		align: "right",
+		negativeMargin: true,
 	},
 	{
 		title: "Seamless Communication",
 		desc: "Enjoy smooth and effortless conversations. Our intuitive design makes communication a breeze.",
 		srcImage: incomingCallImage,
 		align: "left",
+		negativeMargin: true,
 	},
 ]
 
--- src/components/icons/MarkerOne.tsx
+++ src/components/icons/MarkerOne.tsx
@@ -2,8 +2,8 @@ function MarkerOne() {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="50"
-			height="50"
+			width="1em"
+			height="1em"
 			viewBox="0 0 50 50"
 			fill="none"
 		>
